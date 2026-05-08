2d680d2f4dcbc31a5901e1e16f62d491071c523b
3S Dutra -7492073 <raffael.dutra23@gmail.com>
2026-05-08 11:22:47 -0300

    Adicionando funcao de teste concluido

--- arquivo.py
+++ arquivo.py
@@ -1,8 +1,11 @@
 def Mensagem_Boas_Vindas(nome):
     print("Bem vindo ao teste de integração continua!")
 
+def Teste_concluido():
+    print("Teste concluído com sucesso!")
 
 if __name__ == "__main__":
     nome = "Teste Github Actions"
     Mensagem_Boas_Vindas(nome)
-    print("Teste Da Trava")
+    Teste_concluido()
+    
