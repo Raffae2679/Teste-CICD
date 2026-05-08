9a2bb46808e4cdc5a9ed8a54c7ee4fc17aac839c
3S Dutra -7492073 <raffael.dutra23@gmail.com>
2026-05-08 09:08:48 -0300

    Subindo arquivo python de teste

---
+++ arquivo.py
@@ -0,0 +1,7 @@
+def Mensagem_Boas_Vindas(nome):
+    print("Bem vindo ao teste de integração continua!")
+
+
+if __name__ == "__main__":
+    nome = input("Digite seu nome: ")
+    Mensagem_Boas_Vindas(nome)
